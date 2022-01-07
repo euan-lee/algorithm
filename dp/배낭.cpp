af60bce7c65abca74c555620e1a80240fb9b4a38
이범진 (euan) <62791913+euan-lee@users.noreply.github.com>
2022-01-07 11:19:17 +0900

    배낭

--- dp/배낭.cpp
+++ dp/배낭.cpp
@@ -1,52 +1,40 @@
 #include <iostream>
- 
-#define endl "\n"
-#define MAX 110
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <vector>
+
 using namespace std;
- 
-int N, K;
-int Weight[MAX];
-int Value[MAX];
-int DP[MAX][100010];
- 
-int Max(int A, int B) { if (A > B) return A; return B; }
- 
-void Input()
-{
-    cin >> N >> K;
-    for (int i = 1; i <= N; i++)
-    {
-        cin >> Weight[i] >> Value[i];
-    }
-}
- 
-void Solution()
-{
-    for (int i = 1; i <= N; i++)
-    {
-        for (int j = 1; j <= K; j++)
-        {
-            if (j >= Weight[i])    DP[i][j] = Max(DP[i - 1][j], DP[i - 1][j - Weight[i]] + Value[i]);
-            else DP[i][j] = DP[i - 1][j];
-        }
-    }
-    cout << DP[N][K] << endl;
-}
- 
-void Solve()
-{
-    Input();
-    Solution();
-}
- 
-int main(void)
-{
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
- 
-    //freopen("Input.txt", "r", stdin);
-    Solve();
- 
-    return 0;
+
+int w[101]; // 무게
+int v[101]; // 가치
+int dp[101][100001];
+
+int N, K; // 물품의 수 N, 준서가 버틸 수 있는 무게 K
+
+int main() {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+    
+	cin >> N >> K;
+	for (int i = 1; i <= N; i++) {
+		cin >> w[i] >> v[i];
+	}
+    
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= K; j++) {
+			if (j - w[i] >= 0) { // i번째 물건을 넣을 수 있다면?
+				dp[i][j] = max(dp[i-1][j], dp[i - 1][j - w[i]] + v[i]);
+                // 넣지 않을 때와 넣었을 때, 둘 중 더 큰 것으로 초기화
+			}
+            else{ // i번째 물건을 넣을 수 없다면, 배낭 용량은 같고 넣지 않았을 때의 값으로 초기화
+                dp[i][j] = dp[i-1][j];
+            }
+		}
+	}
+
+	cout << dp[N][K];
+
+	return 0;
 }
