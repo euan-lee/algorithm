35326a14cc3483baa78380812c44c790dfed3f76
이범진 (euan) <62791913+euan-lee@users.noreply.github.com>
2022-01-16 22:52:24 +0900

    블랙잭 iteration방식

--- 내가푼 알고리즘/완전탐색/blackjack.cpp
+++ 내가푼 알고리즘/완전탐색/blackjack.cpp
@@ -1,46 +1,24 @@
 #include <iostream>
-#include <cstdio>
-#include <vector>
 using namespace std;
 
-int n,m;
-int arr[1000];
-int copy_arr[1000];
-bool visited[1000];
-int result=0;
-int max1=0;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-void combi(int x){
-    if(x==3){
-    result=copy_arr[0]+copy_arr[1]+copy_arr[2];
-    if(max1<result&&max1<m){
-        max1=result;
-    }
-    return;
-    }  
-    
-    for(int i=0;i<n;i++){
-        if(visited[i]==0){
-            visited[i]=1;
-            copy_arr[x]=arr[i];
-            combi(x+1);
-            visited[i]=0;
-        }
-    }
-}
+	int n, m, result = 0;
+	int arr[101] = {};
+	cin >> n >> m;
+	for (int i = 0; i < n; i++)
+		cin >> arr[i];
 
-
-
-int main(){
-    int max=0;
-    scanf("%d %d",&n,&m);
-    for(int i=0;i<n;i++){
-    scanf("%d",&arr[i]);
-    }
-    for(int i=0;i<n;i++){
-        visited[i]=1;
-        copy_arr[0]=arr[i];
-        combi(1);
-    }
-    printf("%d\n",max1);
+	for (int i = 0; i < n; i++) {
+		for (int j = i + 1; j < n; j++) {
+			for (int k = j + 1; k < n; k++) {
+				if (arr[i] + arr[j] + arr[k] <= m && arr[i] + arr[j] + arr[k] > result)
+					result = arr[i] + arr[j] + arr[k];
+			}
+		}
+	}
+	cout << result << '\n';
 }
