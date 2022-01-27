99a5d9f95c4d89d207f1710587d5a11d435dbdfb
이범진 (euan) <62791913+euan-lee@users.noreply.github.com>
2022-01-27 13:49:24 +0900

    Update 가장긴증가하는부분수열.cpp

--- 내가푼 알고리즘/DP/가장긴증가하는부분수열.cpp
+++ 내가푼 알고리즘/DP/가장긴증가하는부분수열.cpp
@@ -1,54 +1,31 @@
-#include <iostream>
- 
-#define endl "\n"
-#define MAX 1010
+#include <bits/stdc++.h>
 using namespace std;
- 
-int N;
-int Arr[MAX];
-int DP[MAX];
- 
-int Max(int A, int B) { if (A > B) return A; return B; }
- 
-void Input()
-{
-    cin >> N;
-    for (int i = 1; i <= N; i++) cin >> Arr[i];
-}
- 
-void Solution()
-{
-    int Answer = 0;
-    for (int i = 1; i <= N; i++)
-    {
-        DP[i] = 1;
-        for (int j = i - 1; j >= 1; j--)
-        {
-            if (Arr[i] > Arr[j])
-            {
-                DP[i] = Max(DP[i], DP[j] + 1);
-            }
-        }
-        Answer = Max(DP[i], Answer);
-    }
- 
-    cout << Answer << endl;
-}
- 
-void Solve()
-{
-    Input();
-    Solution();
-}
- 
+int dp[1001];
+int arr[1001];
 int main(void)
 {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
- 
-    //freopen("Input.txt", "r", stdin);
-    Solve();
- 
-    return 0;
+	int N;
+	cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		cin >> arr[i];
+		dp[i] = 1;
+	}
+
+	int ans = 1;
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < i; j++)
+		{
+			if (arr[i] > arr[j])
+			{
+				dp[i] = max(dp[i], dp[j] + 1);
+			}
+		}
+		ans = max(ans, dp[i]);
+	}
+
+	cout << ans;
 }
+ 
+
