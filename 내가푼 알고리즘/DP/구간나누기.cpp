4933a86d0c993f78196a002be7018d433b2789c1
이범진 (euan) <62791913+euan-lee@users.noreply.github.com>
2022-01-31 21:29:06 +0900

    Update 구간나누기.cpp

--- 내가푼 알고리즘/DP/구간나누기.cpp
+++ 내가푼 알고리즘/DP/구간나누기.cpp
@@ -1,28 +1,49 @@
 #include <iostream>
+#include <algorithm>
+#define INF 1000000000
 using namespace std;
-#define MIN -3276800; 
-int arr[101]; 
-int dp[101][51]; 
-int calculate(int n, int m) {
-  if (m == 0) return 0; 
-  if (n <= 0) return MIN;
-  if (dp[n][m] != -1) return dp[n][m];
-  int sum = 0; // 3: n번째 인덱스가 구간에 포함되지 않는 경우. 재귀이기 때문에 그 이전의 모든 경우의 수가 계산된다.
-  dp[n][m] = calculate(n - 1, m); 
-  for (int i = n; i > 0; i--) { 
-    // 1: 누적합을 이용해서 1개의 구간을 구한다. (i부터 n까지의 구간)
-    sum += arr[i]; // 2: 인접하지 않는 범위에서 m - 1개의 구간의 합의 최댓값을 구한다.
-    int tmp = calculate(i - 2, m - 1) + sum; 
-    if (tmp > dp[n][m]) { dp[n][m] = tmp; } }
-  return dp[n][m]; } 
-
-int main() { 
-  int n, m; 
-  cin >> n >> m; 
-  for (int i = 1; i <= n; i++) {
-    cin >> arr[i]; 
-    for (int j = 1; j <= n / 2; j++) { 
-      dp[i][j] = -1; 
-    } }
-  cout << calculate(n, m) << '\n'; return 0;
+ 
+int list[101], sum[101], dp[101][51];
+int N, M;
+ 
+int func(int idx, int cnt) {
+    if (cnt == M) return 0;
+    if ((N - idx + 2) / 2 < M - cnt) return -INF;
+ 
+    int &ret = dp[idx][cnt];
+    if (ret != -INF) return ret;
+    ret = func(idx + 1, cnt);
+ 
+    for (int i = idx; i <= N; i++) {
+        ret = max(ret, func(i + 2, cnt + 1) + sum[i] - sum[idx - 1]);
+    }
+ 
+    return ret;
+}
+ 
+void init() {
+    for (int i = 0; i <= N; i++) {
+        for (int j = 0; j <= M; j++) {
+            dp[i][j] = -INF;
+        }
+    }
+}
+ 
+void input() {
+    cin >> N >> M;
+    for (int i = 1; i <= N; i++) {
+        cin >> list[i];
+        sum[i] = sum[i - 1] + list[i];
+    }
+    init();
+}
+ 
+int main() {
+    cin.tie(NULL); cout.tie(NULL);
+    ios::sync_with_stdio(false);
+ 
+    input();
+    cout << func(1, 0) << '\n';
+ 
+    return 0;
 }
