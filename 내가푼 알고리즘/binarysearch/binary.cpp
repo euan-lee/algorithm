18ece772f42ae2a51714c1b37bb58be20ccbaea5
이범진 (euan) <62791913+euan-lee@users.noreply.github.com>
2022-02-06 20:58:46 +0900

    Update binary.cpp

--- 내가푼 알고리즘/binarysearch/binary.cpp
+++ 내가푼 알고리즘/binarysearch/binary.cpp
@@ -4,3 +4,22 @@
 
 
 //while로 작성
+int search(int *arr, int length, int target)
+{
+    int start = 0;
+    int end = length-1;
+    int mid = 0;
+    while (start <= end){
+        mid = (start+end) / 2;
+        if (arr[mid] == target)
+            return mid;
+        else {
+            if (arr[mid] < target)
+                start = mid+1;
+            else if (arr[mid] > target)
+                end = mid-1;
+        }
+    }
+    return -1;
+}
+ 
