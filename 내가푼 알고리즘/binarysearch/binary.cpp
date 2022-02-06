b11d72139245d56fcf8474373e901361b772e9c3
이범진 (euan) <62791913+euan-lee@users.noreply.github.com>
2022-02-06 21:09:16 +0900

    Update binary.cpp

--- 내가푼 알고리즘/binarysearch/binary.cpp
+++ 내가푼 알고리즘/binarysearch/binary.cpp
@@ -1,5 +1,20 @@
 //재귀로 작성(생각하지 못함 ㅠㅠ)
-
+int recursive_search(int *arr, int start, int end, int target)
+{
+    int mid = (start+end) / 2;
+    if (start > end)
+        return -1;
+    else {
+        if (arr[mid] == target)
+            return mid;
+        else {
+            if (arr[mid] < target)
+                recursive_search(arr, mid+1, end, target);
+            else if (arr[mid] > target)
+                recursive_search(arr, start, mid-1, target);
+        }
+    }
+}
 
 
 
