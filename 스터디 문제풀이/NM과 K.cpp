95bc4e5da2b16a91cc86f6512bcb4b7c807e67bd
이범진 (euan) <62791913+euan-lee@users.noreply.github.com>
2022-03-06 23:23:16 +0900

    Update NM과 K.cpp

--- 스터디 문제풀이/NM과 K.cpp
+++ 스터디 문제풀이/NM과 K.cpp
@@ -1,43 +1,49 @@
 #include <iostream>
-#include <bits/stdc++.h>
+#include <vector>
 using namespace std;
+
+bool visited[11][11];
+int arr[11][11];
 int n, m, k;
-int arr[10][10];
-bool check[10][10];
-int ans = -210000000;
-int dx[4] = {-1,0,1,0};
-int dy[4] = {0,1,0,-1};
-void go(int cnt, int sum) {
-    if (cnt == k) {
-        if (ans < sum) ans = sum;
-        return;
-    }
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            if (check[i][j]) continue;
-            bool ok = true;
-            for (int z = 0; z < 4; z++) {
-                int nx = i + dx[z];
-                int ny = j + dy[z];
-                if (nx >= 0 && nx < n && ny >= 0 && ny < m)
-                    if (check[nx][ny]) ok = false;
-            }
-            if (ok) {
-                check[i][j] = true;
-                go(cnt + 1, sum+arr[i][j]);
-                check[i][j] = false;
-            }
-        }
-    }
+int ans =  -40000;
+
+int dx[4] = {0, 0, 1, -1};
+int dy[4] = {1, -1, 0, 0};
+
+void backtrack(int px, int py, int cnt, int sum){
+	if(cnt == k){ //재귀 종료 조건
+		ans = max(ans, sum);
+		return;
+	}
+	for(int x=px; x<n; x++){
+		for(int y=(x == px ? py : 0); y<m; y++){
+			if(!visited[x][y]){
+				bool flag = true;
+				for(int i=0; i<4; i++){ //인접 여부 판단
+					int nx = x + dx[i];
+					int ny = y + dy[i];
+					if(0 <= nx && nx < n && 0 <= ny && ny < m){ //범위 내에 존재 -> 선택 가능한 경우
+						if(visited[nx][ny])	flag = false; //이미 방문한 칸이면 선택 불가
+					}
+				}
+				if(flag){
+					visited[x][y] = true;
+					backtrack(x, y, cnt + 1, sum + arr[x][y]);
+					visited[x][y] = false;
+				}
+			}
+		}
+	}
+
 }
-int main() {
-    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    cin >> n >> m >> k;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            cin >> arr[i][j];
-        }
-    }
-    go(0, 0);
-    cout << ans;
+
+int main(){
+	cin>>n>>m>>k;
+	for(int i=0; i<n; i++){
+		for(int j=0; j<m; j++)
+			cin>>arr[i][j];
+	}
+	backtrack(0, 0, 0, 0);
+	cout<<ans<<'\n';
+	return 0;
 }
