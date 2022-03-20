ca526fdb4ba1ab887bce9666383afe5a5c65fb2a
이범진 (euan) <62791913+euan-lee@users.noreply.github.com>
2022-03-20 16:35:14 +0900

    Update binarysearchtree.js

--- 스터디 문제풀이/binarysearchtree.js
+++ 스터디 문제풀이/binarysearchtree.js
@@ -1,33 +1,25 @@
-ar BSTIterator = function (root) {
-  this.list = []; //  Use an array to store the value of each node 
-  this.index = 0; //  With a pointer , Always point to the next element 
 
-  //  Use middle order to traverse the binary search tree , Save values in order to list
+var BSTIterator = function (root) {
+  this.list = []; 
+
+
   const traversal = (node) => {
-    //  If the node is empty , The exit 
     if (!node) {
       return;
     }
 
-    //  Search the left child node 
     traversal(node.left);
-    //  The node value traversed in the middle order , Deposit in list
     this.list.push(node.val);
-    //  Search right child node 
     traversal(node.right);
   };
-  //  Middle order traversal binary search tree 
   traversal(root);
 };
 
-/** * @return {number} */
 BSTIterator.prototype.next = function () {
-  //  Returns the current value to , And move the pointer back 
   return this.list[this.index++];
 };
 
-/** * @return {boolean} */
 BSTIterator.prototype.hasNext = function () {
-  //  If the value type corresponding to the pointer is number, Indicates that the next item exists , Otherwise undefined
+ 
   return typeof this.list[this.index] === 'number';
 };
