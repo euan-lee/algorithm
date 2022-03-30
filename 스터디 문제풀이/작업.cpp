00f6d3b7958b854de1e12c000ef187408ec62552
이범진 (euan) <62791913+euan-lee@users.noreply.github.com>
2022-03-30 13:34:37 +0900

    Update 작업.cpp

--- 스터디 문제풀이/작업.cpp
+++ 스터디 문제풀이/작업.cpp
@@ -1,34 +1,80 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-using namespace std;
-int N;
-int cost[10001]; // i번째 작업의 작업 소요 시간
-int t[10001]; // i번째 작업이 끝나는 시간 기록
-vector<int> v[10001]; // i번째 작업의 선행 작업 
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<cstring>
+#include<queue>
 
-int main()
+using namespace std;
+
+int n;
+int times[10001]; // 해당 작업 걸리는 시간
+vector<int>PreWork[10001]; // [i]를 선행 작업으로 가지는 노드
+int degree[10001]; // 그래프 차수
+
+void init()
 {
-	cin >> N;
-	for (int cnt, i = 1; i <= N; i++) {
-		cin >> cost[i];
-		cin >> cnt; // i번째 작업의 선행 작업 개수
-		for (int tmp, j = 1; j <= cnt; j++) {
-			cin >> tmp;
-			v[i].push_back(tmp);
+	cin >> n;
+
+	for (int i = 1; i <= n; i++) {
+		int weight, pre_cnt; 
+		// weight: 시간, pre_cnt : 선행 작업 개수
+		
+		cin >> weight;
+		cin >> pre_cnt;
+
+		times[i] = weight;
+
+		degree[i] = pre_cnt;
+
+		for (int j = 0; j < pre_cnt; j++) {
+			int pre_work;
+			cin >> pre_work;
+			PreWork[pre_work].push_back(i);
+		}
+	}
+}
+
+int topology()
+{
+	int result = 987654321;
+
+	// 우선순위 큐 : pair = {걸린 시간, 노드 번호}
+	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>> >pq;
+	
+	// 차수 0인 것들 넣기
+	for (int i = 1; i <= n; i++) {
+		if (degree[i] == 0) {
+			pq.push({ times[i], i });
 		}
 	}
 
-	for (int i = 1; i <= N; i++) {
-		int cur = 0;
-		for (int j = 0; j < v[i].size(); j++) // 현재 작업의 선행 작업 중 가장 늦게 끝나는 작업 시간 구하기
-			cur = max(cur, t[v[i][j]]);
-		t[i] = cur + cost[i];
+	while (!pq.empty())
+	{
+		int w = pq.top().first;
+		int node = pq.top().second;
+		pq.pop();
+
+		result = w;
+
+		for (int i = 0; i < PreWork[node].size(); i++) {
+			int nextnode = PreWork[node][i];
+			degree[nextnode]--;
+
+			if (degree[nextnode] == 0) {
+				pq.push({ w + times[nextnode], nextnode });
+			}
+		}
 	}
 
-	int res = 0; //결과 구하기
-	for (int i = 1; i <= N; i++) 
-		res = max(res, t[i]);
+	return result;
+}
 
-	cout << res;
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	init();
+	cout << topology();
 }
