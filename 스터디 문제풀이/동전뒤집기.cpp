c0d781659492302293439672d4de10618c1e5745
이범진 (euan) <62791913+euan-lee@users.noreply.github.com>
2022-04-03 21:01:54 +0900

    Update 동전뒤집기.cpp

--- 스터디 문제풀이/동전뒤집기.cpp
+++ 스터디 문제풀이/동전뒤집기.cpp
@@ -1,74 +1,73 @@
 #include <iostream>
-#include <vector>
-#include <cstring>
-#include <fstream>
 #include <algorithm>
-#include <cmath>
-#define endl '\n'
-
+#include <vector>
+#include <string>
+#include <queue>
+#include <map>
 using namespace std;
 
 int n;
-string str[20];
-int dp[1048576];
+vector<string> board;
+int result = 987654321;
 
-int countT(){
-  int ret = 0;
-  for (int i=0; i<n; i++){
-    int cnt = 0;
-    for (int j=0; j<n; j++)
-      if (str[j][i] == 'T')
-        cnt++;
-    // T가 더 많으면 뒤집은 결과 저장
-    ret += min(cnt, n-cnt);
-  }
+int Solve(int row_status)
+{
+	int ret = 0;
+	for (int j = 0; j < n; j++)
+	{
+		int h_cnt = 0; //해당 행에서 앞면의 개수
+		int t_cnt = 0; //해당 행에서 뒷면의 개수
 
-  return ret;
+		for (int i = 0; i < n; i++)
+		{
+			//해당 행이 뒤집혀 있을 경우
+			if (((row_status >> i) & 1) == 1)
+			{
+				if (board[i][j] == 'H')
+					t_cnt++;
+				else
+					h_cnt++;
+			}
+
+			else
+			{
+				if (board[i][j] == 'H')
+					h_cnt++;
+				else
+					t_cnt++;
+			}
+		}
+
+		//j열에서 앞면의 개수와 뒷면의 개수중 작은것을 더하면 된다
+		//만약 앞면의 개수가 더 작다면 j열을 뒤집는다고 생각하면 되기 때문이다 
+		ret += min(h_cnt, t_cnt);
+	}
+
+	return ret;
 }
 
-void reverse(int line){
-  for (int i=0; i<n; i++){
-    if (str[line][i] == 'T') str[line][i] = 'H';
-    else str[line][i] = 'T';
-  }
-}
+int main()
+{
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
 
-int solve(int check){
-  
-  int &ret = dp[check];
-  if (ret != -1) return ret;
+	cin >> n;
 
-  ret = countT();
-  
-  for (int next=0; next<n; next++){
-    ret = min(ret, solve(check));
-  }
+	for (int i = 0; i < n; i++)
+	{
+		string input;
+		cin >> input;
 
-  for (int next=0; next<n; next++){
-    reverse(next);
-    ret = min(ret, solve(check|(1<<next)));
-    reverse(next);
-  }
+		board.push_back(input);
+	}
 
-  return ret;
-}
+	//각 행들의 뒤집힌 경우를 모두 확인한다
+	for (int row_status = 0; row_status < (1 << n) - 1; row_status++)
+	{
+		result = min(result, Solve(row_status));
+	}
 
-int main(){
-  ios_base :: sync_with_stdio(false);
-  cin.tie(NULL);
+	cout << result;
 
-  cout.tie(NULL);
-
-  // ifstream cin;
-  // cin.open("input.txt");
-
-  cin >> n;
-
-  for (int i=0; i<n; i++){
-    cin >> str[i];
-  }
-
-  memset(dp,-1,sizeof(dp));
-
-  cout << solve(0);
+	return 0;
 }
