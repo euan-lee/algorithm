e825fdb25ca2c7bb5744b12769d5d0525080485b
(euan) <62791913+euan-lee@users.noreply.github.com>
2022-04-30 16:15:37 +0900

    Create 점프와 순간이동.cpp

---
+++ 스터디 문제풀이/점프와 순간이동.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include<bitset>
+using namespace std;
+
+int solution(int n){
+    int answer = 0;
+    while (n != 0) {
+        if (n % 2 == 1)answer++;
+        n /= 2;
+    }
+    return answer;
+}
