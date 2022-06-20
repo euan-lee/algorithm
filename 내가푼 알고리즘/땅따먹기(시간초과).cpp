6c1f19ed39abbbcb01227b6f54a7a215c304b37a
(euan) <62791913+euan-lee@users.noreply.github.com>
2022-06-20 18:33:51 +0900

    Update 땅따먹기(시간초과).cpp

--- 내가푼 알고리즘/땅따먹기(시간초과).cpp
+++ 내가푼 알고리즘/땅따먹기(시간초과).cpp
@@ -4,16 +4,20 @@
 #include<string.h> 
 using namespace std;
 int row[4];
- int answer;
+int answer;
+
 void dfs(int n,vector<vector<int> >land1){
     if(n==land1.size()){
         int sum=0;
         for(int i=0;i<land1.size();i++){
             int y=row[i];
             int x=i;
+          //  printf("row [%d]:%d",i,row[i]);
+           // printf("land1[%d][%d]:%d\n",x,y,land1[x][y]);
             sum=sum+land1[x][y];
         }
         answer=max(answer,sum);
+      //  printf("sum:%d\n",answer);
         return;
     }
     for(int i=0;i<land1[0].size();i++){
@@ -23,7 +27,8 @@ void dfs(int n,vector<vector<int> >land1){
                 dfs(n+1,land1); 
                 row[n]=-1;
             }       
-        }else if(n==0){
+        }
+        else if(n==0){
             if(row[n]==-1){
                 row[n]=i;
                 dfs(n+1,land1); 
