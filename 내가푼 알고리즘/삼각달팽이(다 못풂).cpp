fba301c3b9ce003b4feb2d4a0bd82097c8042f29
(euan) <62791913+euan-lee@users.noreply.github.com>
2022-06-27 19:01:46 +0900

    Update 삼각달팽이(다 못풂).cpp

--- 내가푼 알고리즘/삼각달팽이(다 못풂).cpp
+++ 내가푼 알고리즘/삼각달팽이(다 못풂).cpp
@@ -1,33 +1,62 @@
 #include <string>
 #include <vector>
+#include <map>
 using namespace std;
-int arr[1000];
+int arr[10000];
 
-void Outside(int n,int start,int start_value){
-    if(n<0){
+void Outside(int original_size,int triangle_size,int start,int start_value,int depth){
+    if(triangle_size<=0){
         return;
     }
     arr[start]=start_value;
-    printf("start:%d\n",start);
-    for(int i=start+1;i<=start+n-1;i++){
-        arr[i]=arr[i-1]+i;
+    for(int i=start+1;i<start+1+triangle_size-1;i++){
+        arr[i]=arr[i-1]+(1+2*(depth-1))+i-(start+1);
     }
-    for(int i=start+n;i<start+2*n-1;i++){
+
+    for(int i=start+1+triangle_size-1;i<start+1+2*(triangle_size-1);i++){
         arr[i]=arr[i-1]+1;
     }
-    for(int i=start+2*n-1;i<start+n*(n+1)/2;i++){
-        arr[i]=arr[i-1]-1;
+    
+    for(int i=start+1+2*(triangle_size-1);i<start+3*(triangle_size-1);i++){
+        arr[i]=arr[i-1]-((original_size-depth+1)-(i-(start+1+2*(triangle_size-1))));
     }
-    for(int i=0;i<n*(n+1)/2;i++){
-        printf("%d ",arr[i]);
-    }
-    Outside(n-3,1+3*(n-1),);
+    Outside(original_size,triangle_size-3,start+3*(triangle_size-1),start_value+4,depth+1);
 }
-vector<int> solution(int n) {
+vector<int> solution(int n){ 
+    //n=10;
     vector<int> answer;
-    answer.push_back(0);
-    arr[1000]={0,};
-    //arr[n]은 n+1이 배열의 어느 위치에 있는지?이다.
-    Outside(n,0);
+    map<int,int> m;
+    arr[10000]={0,};
+    Outside(n,n,0,1,1);
+    for(int i=0;i<10000;i++){
+        if(arr[i]==0){
+            break;
+        }
+         printf("%d ",arr[i]);
+        m.insert({arr[i],i+1});
+    }
+    for(int i=1;i<=m.size();i++){
+        answer.push_back(m[i]);
+    }
+    
     return answer;
 }
+
+
+
+/*
+삼각형 9->1(1+2*(depth-1)),2,3,4순으로 증가 ->오른쪽으로 가면서 1씩 증가->9(9-depth+1) 8 7 6 5 순으로 감소 depth=1
+삼각형 6->3(1+2*(depth-1)),4,5,6 순으로 증가->오른쪽으로 가면서 1씩 증가->8(9-depth+1) 7 6 5 순으로 감소 depth=2
+삼각형 3->5(1+2*(depth-1)),6,7,8 순으로 증가->오른쪽으로 가면서 1씩 증가->7(n_real-depth+1) 6 5 순으로 감소 depth=3
+
+                1(1)
+            2(2) 24(3)
+          3(4) 25(5) 23(6)
+        4(7) 26(8) 39(9) 22(10)   
+      5(11) 27(12) 40(13) 38(14) 21(15)   
+   6(16) 28(17) 41(18) 45(19) 37(20) 20(21)
+  7(22) 29(23) 42(24) 43(25) 44(26) 36(27) 19(28)
+ 8(29) 30(30) 31(31) 32(32) 33(33) 34(34) 35(35) 18(36)  
+9(37) 10(38) 11(39) 12(40) 13(41) 14(42) 15(43) 16(44) 17(45)
+
+*/
