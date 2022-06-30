b69d9fb08ff2015bd0931b6f4ca7ac8db6cbb60d
(euan) <62791913+euan-lee@users.noreply.github.com>
2022-06-30 21:05:19 +0900

    못풂(일요일에 마저 풀기)

--- 내가푼 알고리즘/후보키.cpp
+++ 내가푼 알고리즘/후보키.cpp
@@ -9,19 +9,22 @@ int visited[20];
 bool Check(int n,int r,int cnt,vector<vector<string>> &relation){
     if(cnt==r){   
         unordered_map<string,int> mile_stone;
-        printf("hash에 넣음\n");
-        for(int i=0;i<r;i++){
-            for(int k=0;k<relation.size();k++){
-                printf("%s\n",relation[k][row[i]].c_str());
-                mile_stone[k]=1;    
+        for(int k=0;k<relation.size();k++){
+            string temp="";
+            for(int i=0;i<r;i++){
+                temp=temp+relation[k][row[i]];
             }
+            mile_stone.insert({temp,1}); 
+            printf("%s:%d\n",temp.c_str(),mile_stone[temp]); 
         }
-        for(int i=0;i<mile_stone.size();i++){
-        
+        printf("milesstone size is%d\n",mile_stone.size());
+        printf("relation size is%d\n",relation.size());
+        if(mile_stone.size()==relation.size()){
             return true;
         }
-        printf("-----");
-        return false;
+        else{
+          return false; 
+        }
     }
     for(int i=0;i<n;i++){
         if(visited[i]==0){
@@ -39,6 +42,7 @@ int CombinationAll(int n,vector<vector<string>> &relation){
     bool flag=false;
     for(int i=0;i<n;i++){
         flag=Check(n,i+1,0,relation);
+        printf("check falg:%d %d\n",i,flag);
         if(flag==true){
             return i+1;   
         }
