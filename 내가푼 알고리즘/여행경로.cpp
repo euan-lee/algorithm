1dd0eeeaff05d9122e4800df6d7eb48d8c76024e
(euan) <62791913+euan-lee@users.noreply.github.com>
2022-07-13 18:35:14 +0900

    Update 여행경로.cpp

--- 내가푼 알고리즘/여행경로.cpp
+++ 내가푼 알고리즘/여행경로.cpp
@@ -3,37 +3,35 @@
 #include <algorithm>
 using namespace std;
 int last;
-void Dfs(int way_num,vector<int> &visited,vector<vector<string>> &tickets,vector<string>&answer){
-    last=way_num;
-    visited[way_num]=1;
-    answer.push_back(tickets[way_num][0]);
+int cnt;
+bool flag;
+
+void Dfs(string way,vector<int> &visited,vector<vector<string>> &tickets,vector<string>&answer){
+    if(cnt==(int)tickets.size()) flag=true;
     for(int i=0;i<tickets.size();i++){
-        if(visited[i]!=1){
-            if(tickets[way_num][1]==tickets[i][0]){
-                Dfs(i,visited,tickets,answer);  
-            }  
+        if(visited[i]==0&&way==tickets[i][0]){
+            answer.push_back(tickets[i][1]);
+            cnt++;    
+            visited[i]=1;
+            Dfs(tickets[i][1],visited,tickets,answer);  
+            if(flag==false){
+                answer.pop_back();
+                visited[i]=0;
+            }
         }
     }
 }
 vector<string> solution(vector<vector<string>> tickets) {
-    tickets={{"ICN","B"}, {"B","C"},{"C","ICN"},{"ICN","D"},{"ICN","E"}, {"E","F"}};
-    last=0;
+    flag = false;
+    cnt=0;
     vector<string> answer;
     vector<int> visited;
-    string start=tickets[0][0];
     int index=0;
-    sort(tickets.begin(),tickets.end());
+    sort(tickets.begin(), tickets.end());
     for(int i=0;i<tickets.size();i++){
         visited.push_back(0);
     }
-    
-    for(int i=0;i<tickets.size();i++){
-        if(start==tickets[i][0]){
-            index=i;
-            break;
-        }
-    }
-    Dfs(index,visited,tickets,answer);
-    answer.push_back(tickets[last][1]);
+    answer.push_back("ICN");
+    Dfs("ICN",visited,tickets,answer);
     return answer;
 }
