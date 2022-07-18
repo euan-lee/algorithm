1e92c4d6d292f62e7cf007143cc18bcf30c0dc44
(euan) <62791913+euan-lee@users.noreply.github.com>
2022-07-18 20:10:29 +0900

    Update 퍼즐조각채우기.cpp
    dfs부분 수정하기

--- 내가푼 알고리즘/퍼즐조각채우기.cpp
+++ 내가푼 알고리즘/퍼즐조각채우기.cpp
@@ -2,31 +2,61 @@
 #include <vector>
 #include <queue>
 using namespace std;
-/*
-void dfs(vector<vector<int>> &game_board,vector<vector<int>> &blocks,vector<int> &visited,int depth){
+
+int In_boundary(vector<pair<int,int>> &block,vector<vector<int>> &game_board,int r,int c){
+  for(int i=0;i<block.size();i++){
+      int x=block[i].first+r;
+      int y=block[i].second+c;
+      if(x>=0&&x<block.size()&&y>=0&&y<block[i].size()){
+          continue;     
+      }else{
+          return 0;      
+      }   
+  }     
+    return 1;
+}
+
+void Rotation(vector<pair<int,int>> &block){
+    for(int i=0;i<block.size();i++){
+        block[i].first=-block[i].second;
+        block[i].second=block[i].first;
+    }
+}
+
+
+void dfs(vector<vector<int>> &game_board,vector<vector<pair<int,int>>> blocks;,vector<int> &visited,int depth){
     
     if(blocks.size()==depth){
-        return ;
+        return;
     }
-    
     for(int i=0;i<game_board.size();i++){//게임 보드 위치 순회
         for(int j=0;j<game_board[i].size();j++){//게임 보드 위치 순회
-            
-            for(int k=0;k<visited.size();k++){//테이블 사용여부 확인을 위해서
-                
-                if(game_board[i][j]!=1&&visited[k]!=1){//만약 게임 보드가 비어있고 블럭을 쓸수 있다면~
-                    for(int h=0;h<4;h++){//회전한 후  
+            for(int k=0;k<blocks.size();k++){//테이블 사용여부 확인을 위해서
+                if(game_board[i][j]!=1&&visited[k]==0){//만약 게임 보드가 비어있고 블럭을 쓸수 있다면~
+                    for(int h=0;h<4;h++){//회전한 후
+                        Rotation(blocks[k]);
+                        if(In_boundary(blocks[k],game_board,i,j)){//범위 안에 들어온다면 
+                           //game_board에 block을 위치시킴
+                            for(int m=0;m<blocks[k].size();m++){
+                            
+                            }
+                            //
+                        }
                         visited[k]=1;
-                        //table[k] 회전변환
-                        //game_board[i][j]==table[k]
-                        dfs(game_board,table,visited,depth++);
+                        dfs(game_board,table,visited,depth+1);
+                        visited[k]=0;
+                        //game_board에 block을 원위치 시킴
+                        for(int m=0;m<blocks[k].size();m++){
+                            
+                        }
+                        //
                     } 
                 }
             }
         }
     }
 }
-*/
+
 
 bool CheckMap(int x, int y,int r,int c){
     return (x >= 0 && x < r) && (y >=0 && y < c);
@@ -53,7 +83,8 @@ void Bfs(int x,int y,vector<vector<int>> &table,vector<pair<int,int>> &block){
 }
 
 int solution(vector<vector<int>> game_board, vector<vector<int>> table) {
-    //bfs
+    
+    //1.bfs
     vector<vector<pair<int,int>>> blocks;   
     int cnt=-1;
     for(int i=0;i<table.size();i++){
@@ -62,22 +93,14 @@ int solution(vector<vector<int>> game_board, vector<vector<int>> table) {
                 vector<pair<int,int>> block;   
                 Bfs(i,j,table,block);//table의 블록 나누기(1,1을 기준으로 좌표들을 구한다)    
                 blocks.push_back(block);
-                printf("\n");
             } 
         }
     }
-    //check
-   for(int i=0;i<blocks.size();i++){
-        for(int j=0;j<blocks[i].size();j++){
-            printf("(%d %d) ",blocks[i][j].first,blocks[i][j].second);    
-        } 
-        printf("\n");
-    }
     
-    //dfs    
+    //2.dfs    
     int answer = -1; 
     vector<int> visited(table.size());//table.size()만큼 사이즈 0으로 할당
-    //dfs(game_board,table,visited,0);
+    dfs(game_board,table,visited,0);
     
     return answer;
 }
