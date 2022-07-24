53fb5f585c1ecc0d30203f05a2f736ef20f51c16
(euan) <62791913+euan-lee@users.noreply.github.com>
2022-07-24 20:49:50 +0900

    퍼즐 조각 맞추기
    dfs부분 손보기

--- 내가푼 알고리즘/퍼즐조각채우기.cpp
+++ 내가푼 알고리즘/퍼즐조각채우기.cpp
@@ -3,104 +3,103 @@
 #include <queue>
 using namespace std;
 
-int In_boundary(vector<pair<int,int>> &block,vector<vector<int>> &game_board,int r,int c){
-  for(int i=0;i<block.size();i++){
-      int x=block[i].first+r;
-      int y=block[i].second+c;
-      if(x>=0&&x<block.size()&&y>=0&&y<block[i].size()){
-          continue;     
-      }else{
-          return 0;      
-      }   
-  }     
-    return 1;
-}
-
-void Rotation(vector<pair<int,int>> &block){
-    for(int i=0;i<block.size();i++){
-        block[i].first=-block[i].second;
-        block[i].second=block[i].first;
-    }
-}
-
-
-void dfs(vector<vector<int>> &game_board,vector<vector<pair<int,int>>> blocks;,vector<int> &visited,int depth){
-    
-    if(blocks.size()==depth){
-        return;
-    }
-    for(int i=0;i<game_board.size();i++){//게임 보드 위치 순회
-        for(int j=0;j<game_board[i].size();j++){//게임 보드 위치 순회
-            for(int k=0;k<blocks.size();k++){//테이블 사용여부 확인을 위해서
-                if(game_board[i][j]!=1&&visited[k]==0){//만약 게임 보드가 비어있고 블럭을 쓸수 있다면~
-                    for(int h=0;h<4;h++){//회전한 후
-                        Rotation(blocks[k]);
-                        if(In_boundary(blocks[k],game_board,i,j)){//범위 안에 들어온다면 
-                           //game_board에 block을 위치시킴
-                            for(int m=0;m<blocks[k].size();m++){
-                            
-                            }
-                            //
-                        }
-                        visited[k]=1;
-                        dfs(game_board,table,visited,depth+1);
-                        visited[k]=0;
-                        //game_board에 block을 원위치 시킴
-                        for(int m=0;m<blocks[k].size();m++){
-                            
-                        }
-                        //
-                    } 
+void Bfs(int x, int y,vector<vector<bool>>&visited,vector<vector<int>>&table,vector<pair<int,int>> &block){
+    queue< pair<int,int> > q; 
+    q.push(make_pair(x,y)); 
+    visited[x][y] = true;
+    int dx[] = {-1, 0, 1, 0};
+    int dy[] = {0, 1, 0, -1};
+    int st_x=x;
+    int st_y=y;
+    while(!q.empty()){
+        x = q.front().first;
+        y = q.front().second;
+        q.pop();
+        block.push_back(make_pair(x-st_x,y-st_y));
+        for(int i=0;i<4; i++){
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if(0<=nx&&nx<table.size()&&0<=ny&&ny<table[0].size()){
+                if(table[nx][ny] == 1 && visited[nx][ny] == false){
+                    visited[nx][ny]=true;
+                    q.push(make_pair(nx,ny));   
                 }
             }
         }
     }
 }
 
-
-bool CheckMap(int x, int y,int r,int c){
-    return (x >= 0 && x < r) && (y >=0 && y < c);
-}
-
-void Bfs(int x,int y,vector<vector<int>> &table,vector<pair<int,int>> &block){
-    int dir[4][2]={{-1,0},{1,0},{0,-1},{0,1}};     
-    queue<pair<int,int>> q; 
-    q.push(pair<int,int>(x,y)); 
-    while(!q.empty()){
-        int cur_r=q.front().first;        
-        int cur_c=q.front().second; 
-        q.pop();     
-        for(int i=0;i<4;i++){
-            int next_r=cur_r+dir[i][0];
-            int next_c=cur_c+dir[i][1];
-            if(CheckMap(next_r,next_c,table.size(),table[i].size())&&table[next_r][next_c]==1){
-                table[next_r][next_c]=0;
-                block.push_back({make_pair(next_r-x,next_c-y)});
-                q.push(pair<int,int>(next_r,next_c));  
-            }
-        } 
+void Rotation(vector<pair<int,int>> &block){
+    for(int i=0;i<block.size();i++){
+        int temp=block[i].first;
+        block[i].first=-1*block[i].second;
+        block[i].second=temp;
     }
 }
+
+bool FitCheck(int x,int y,vector<vector<int>> &game_board,vector<pair<int,int>> &block){
+    printf("cord %d %d\n",x,y);
+    printf("block-size:%d\n",block.size());
+    for(int i=0;i<block.size();i++){//블럭[i]가 게임 보드 안에 들어있고 
+            if(x+block[i].first>=0&&y+block[i].second>=0&&game_board[i].size()>x+block[i].first&&game_board[i].size()>y+block[i].second){
+            }
+            else{
+                return false;
+            }
+        }
+        for(int i=0;i<block.size();i++){//블럭이 들어갈수 있다면
+            if(game_board[x+block[i].first][y+block[i].second]==1){//없다면
+                return false;
+           }    
+        }
+    return true;
+}
+void FillBlock(int x,int y,vector<vector<int>> &game_board,vector<pair<int,int>> &block){
+    
+}
+
 
 int solution(vector<vector<int>> game_board, vector<vector<int>> table) {
-    
-    //1.bfs
+   
+    //1.bfs 로 블록 구하기 ->시간복잡도 O(n*n) n=2500
+    //이차원 visited vector를 table 만큼 생성
+    vector<vector<bool>> visited(table.size(),vector<bool>(table[0].size())); 
     vector<vector<pair<int,int>>> blocks;   
-    int cnt=-1;
     for(int i=0;i<table.size();i++){
-        for(int j=0;j<table[i].size();j++){
-            if(table[i][j]==1){
+        for(int j=0;j<table[0].size();j++){
+            if(visited[i][j]==0&&table[i][j]==1){
                 vector<pair<int,int>> block;   
-                Bfs(i,j,table,block);//table의 블록 나누기(1,1을 기준으로 좌표들을 구한다)    
+                Bfs(i,j,visited,table,block);    
                 blocks.push_back(block);
-            } 
+            }
+             
         }
     }
+    //visited_dfs->blocks사이즈만큼(최대 6개까지)
+    vector<bool> visited_dfs(blocks.size(),false); 
     
-    //2.dfs    
-    int answer = -1; 
-    vector<int> visited(table.size());//table.size()만큼 사이즈 0으로 할당
-    dfs(game_board,table,visited,0);
+    //dfs 처리(블럭의 순서도를 permutation으로 구한다.)
+    for(int i=0;i<game_board.size();i++){//게임 보드 탐색하며 
+        for(int j=0;j<game_board[i].size();j++){
+            if(game_board[i][j]==0)
+            for(int k=0;k<blocks.size();k++){//모든 블럭을 그 자리에 넣어본다.
+                if(visited_dfs[k]==0){//만약 블럭k가 사용되지 않았고
+                        for(int h=0;h<4;h++){
+                            Rotation(blocks[k]);//k를 회전했을 떄
+                            if(FitCheck(i,j,game_board,blocks[k])){//i,j자리에 맞는다면
+                                //게임보드를 블럭자리에 1로 변경
+                                FillBlock((i,j,game_board,blocks[k]));
+                               // Permutation(i,j,visited_dfs,blocks);         
+
+                                //게임보드를 블럭자리에 0으로 변경
+                            }
+                             printf("rotation end\n");
+                        }
+                }
+            }       
+        }    
+    }
     
+    int answer = 14;
     return answer;
 }
